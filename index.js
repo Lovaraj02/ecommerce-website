56555682b34ef52f5cbde776eabdf90b07673fb7
raju <rajubandaru2255@gmail.com>
2024-12-31 23:07:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,128 @@
+function cart()
+{
+    alert("items levu bro   ");
+}
+function noise()
+{
+    alert("details of the product");
+    alert("price --> 999");
+    alert("7 days return polocy awailable");
+}
+function cam()
+{
+    alert("details of the product");
+    alert("price --> 1,999");
+    alert("7 days return polocy awailable");
+}
+function mobile()
+{
+    alert("details of the product");
+    alert("price --> 35,999");
+    alert("8gb 256");
+    alert("7 days return polocy awailable");
+}
+function laptop()
+{
+    alert("details of the product");
+    alert("price --> 49,999");
+    alert("7 days return polocy awailable");
+}
+function watch()
+{
+    alert("details of the product");
+    alert("price --> 1,499");
+    alert("7 days return polocy awailable");
+}
+function fridge()
+{
+    alert("details of the product");
+    alert("price --> 31,999");
+    alert("7 days return polocy awailable");
+}
+function beauty()
+{
+    alert("details of the product");
+    alert("price --> 699");
+    alert("7 days return polocy awailable");
+}
+function trimmer()
+{
+    alert("details of the product");
+    alert("price --> 1099");
+    alert("7 days return polocy awailable");
+}
+
+// 
+function mat()
+{
+    alert("details of the product");
+    alert("price --> 349");
+    alert("7 days return polocy awailable");
+}
+function saree()
+{
+    alert("details of the product");
+    alert("price --> 599");
+    alert("7 days return polocy awailable");
+}
+function slippers()
+{
+    alert("details of the product");
+    alert("price --> 299");
+    alert("7 days return polocy awailable");
+}
+function dress()
+{
+    alert("details of the product");
+    alert("price --> 799");
+    alert("7 days return polocy awailable");
+}
+function sleeves()
+{
+    alert("details of the product");
+    alert("price --> 199");
+    alert("7 days return polocy awailable");
+}
+function sweat_shirt()
+{
+    alert("details of the product");
+    alert("price --> 999");
+    alert("7 days return polocy awailable");
+}
+function perfume()
+{
+    alert("details of the product");
+    alert("price --> 499");
+    alert("7 days return polocy awailable");
+}
+function bat()
+{
+    alert("details of the product");
+    alert("price --> 1299");
+    alert("7 days return polocy awailable");
+}
+function cap()
+{
+    alert("details of the product");
+    alert("price --> 899");
+    alert("7 days return polocy awailable");
+}
+function shirt()
+{
+    alert("details of the product");
+    alert("price --> 499");
+    alert("7 days return polocy awailable");
+}
+function glasses()
+{
+    alert("details of the product");
+    alert("price --> 299");
+    alert("7 days return polocy awailable");
+}
+
+function baniyan()
+{
+    alert("details of the product");
+    alert("price --> 99");
+    alert("7 days return polocy awailable");
+}
